e0065e38b4d6bfe2e2bf0aceb2340c11c8f89152
Hassan Kashif. pro <hassan.kashif.pro@Gmail.com>
2024-08-10 23:22:41 +0500

    (webapp): Initial Files and Routes Setup.

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/sign-in" Component={SignIn} />
+        <Route path="/sign-up" Component={SignUp} />
+      </Routes>
+    </Router>
   );
+}
+
+function Home() {
+  return <h1>Home Page</h1>;
+}
+
+function SignIn() {
+  return <h1>Sign In Page</h1>;
+}
+
+function SignUp() {
+  return <h1>Sign Up Page</h1>;
 }
 
 export default App;
