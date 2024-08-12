00f68bb7d3fc66c048e17507b38b6801dec6bd21
Hassan Kashif. pro <hassan.kashif.pro@Gmail.com>
2024-08-12 20:45:52 +0500

    (webapp): Configure Files and Set Up Figma Design.

--- webapp/src/components/Navbar.tsx
+++ webapp/src/components/Navbar.tsx
@@ -1,22 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Navbar as NavbarBs, Nav, Container } from "react-bootstrap";
+import { Navbar as NavbarBs, Nav, Container, Button } from "react-bootstrap";
+import "./Navbar.css"; // Import the CSS file for additional styling
+import SignIn from "../pages/SignIn";
 
 const Navbar: React.FC = () => {
   return (
     <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
       <Container>
-        <Nav className="mr-auto">
-          <Nav.Link as={Link} to="/">
+        <NavbarBs.Brand href="/" className="navbar-brand">
+          {/* Placeholder for logo */}
+          <img
+            src="/logo.png" // Change this to the path of your logo
+            alt="Logo"
+            height="40"
+          />
+        </NavbarBs.Brand>
+        <Nav className="me-auto">
+          <Nav.Link as={Link} to="/" className="nav-link-spacing">
             Home
           </Nav.Link>
-          <Nav.Link as={Link} to="/sign-in">
+          <Nav.Link as={Link} to="/sign-in" className="nav-link-spacing">
             Sign In
           </Nav.Link>
-          <Nav.Link as={Link} to="/sign-up">
+          <Nav.Link as={Link} to="/sign-up" className="nav-link-spacing">
             Sign Up
           </Nav.Link>
         </Nav>
+        <Button
+          onClick={SignIn}
+          variant="outline-success"
+          className="sign-in-button"
+        >
+          Sign In
+        </Button>
       </Container>
     </NavbarBs>
   );
