1b34e5efa0da53aad25d7b88c8b59c69c0654ab2
Hassan Kashif. pro <hassan.kashif.pro@Gmail.com>
2024-08-13 23:46:17 +0500

    (webapp):Added Logo to Navbar.

--- webapp/src/components/Navbar.tsx
+++ webapp/src/components/Navbar.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Navbar as NavbarBs, Nav, Container, Button } from "react-bootstrap";
-import "./Navbar.css"; // Import the CSS file for additional styling
 import SignIn from "../pages/SignIn";
 
 const Navbar: React.FC = () => {
@@ -10,11 +9,7 @@ const Navbar: React.FC = () => {
       <Container>
         <NavbarBs.Brand href="/" className="navbar-brand">
           {/* Placeholder for logo */}
-          <img
-            src="/logo.png" // Change this to the path of your logo
-            alt="Logo"
-            height="40"
-          />
+          <img src="public\Logo.png" alt="Logo" height="40" p-2 />
         </NavbarBs.Brand>
         <Nav className="me-auto">
           <Nav.Link as={Link} to="/" className="nav-link-spacing">
