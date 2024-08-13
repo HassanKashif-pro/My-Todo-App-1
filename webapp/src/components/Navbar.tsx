7cad9c70b1a8c1513fd35d7dd442567736de96b7
Hassan Kashif. pro <hassan.kashif.pro@Gmail.com>
2024-08-14 01:12:18 +0500

    (webapp)-(Fix): External Css Styles not importing...Fixed for some Time.

--- webapp/src/components/Navbar.tsx
+++ webapp/src/components/Navbar.tsx
@@ -1,7 +1,9 @@
+// Navbar.tsx
 import React from "react";
 import { Link } from "react-router-dom";
 import { Navbar as NavbarBs, Nav, Container, Button } from "react-bootstrap";
 import SignIn from "../pages/SignIn";
+import "./Navbar.css"; // Import the new CSS file
 
 const Navbar: React.FC = () => {
   return (
@@ -9,7 +11,7 @@ const Navbar: React.FC = () => {
       <Container>
         <NavbarBs.Brand href="/" className="navbar-brand">
           {/* Placeholder for logo */}
-          <img src="public\Logo.png" alt="Logo" height="40" p-2 />
+          <img src="/public/Logo.png" alt="Logo" height="40" p-2 />
         </NavbarBs.Brand>
         <Nav className="me-auto">
           <Nav.Link as={Link} to="/" className="nav-link-spacing">
