45fe838c7a9b11338502dfb2432f3023f813d69c
Hassan Kashif. pro <hassan.kashif.pro@Gmail.com>
2024-08-18 00:00:24 +0500

    (webapp): Navbar Link

--- webapp/src/components/Navbar.tsx
+++ webapp/src/components/Navbar.tsx
@@ -1,34 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Navbar as NavbarBs, Nav, Container, Button } from "react-bootstrap";
+import { Navbar as NavbarBs, Nav, Button } from "react-bootstrap";
 import "../style/Navbar.css"; // Import the new CSS file
 
 const Navbar: React.FC = () => {
   return (
-    <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
-      <Container className="d-flex justify-content-between align-items-center">
-        <NavbarBs.Brand href="/" className="navbar-brand">
-          <img src="/Logo.png" alt="Logo" className="navbar-logo" />
-        </NavbarBs.Brand>
-        <Nav className="d-flex">
-          <Nav.Link as={Link} to="/" className="nav-link-spacing">
-            Home
-          </Nav.Link>
-          <Nav.Link as={Link} to="/sign-in" className="nav-link-spacing">
-            Sign In
-          </Nav.Link>
-          <Nav.Link as={Link} to="/sign-up" className="nav-link-spacing">
-            Sign Up
-          </Nav.Link>
-        </Nav>
-        <Button
-          onClick={() => (window.location.href = "/sign-in")}
-          variant="outline-success"
-          className="custom-sign-in-button ms-auto"
-        >
+    <NavbarBs sticky="top" className="bg-white shadow-sm mb-3 me-auto">
+      <NavbarBs.Brand href="/" className="navbar-brand">
+        <img src="/Logo.png" alt="Logo" className="navbar-logo" />
+      </NavbarBs.Brand>
+      <Nav className="d-flex">
+        <Nav.Link as={Link} to="/" className="nav-link">
+          Home
+        </Nav.Link>
+        <Nav.Link as={Link} to="/sign-in" className="nav-link">
           Sign In
-        </Button>
-      </Container>
+        </Nav.Link>
+        <Nav.Link as={Link} to="/sign-up" className="nav-link">
+          Sign Up
+        </Nav.Link>
+      </Nav>
+      <Button
+        onClick={() => (window.location.href = "/sign-in")}
+        variant="outline-success"
+        className="custom-sign-in-button"
+      >
+        Sign In
+      </Button>
     </NavbarBs>
   );
 };
