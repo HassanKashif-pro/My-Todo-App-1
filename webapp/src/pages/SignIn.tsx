12a59a4fc27df4a607e63c04cbb5624d055a1ee1
Hassan Kashif. pro <hassan.kashif.pro@Gmail.com>
2024-08-20 23:08:56 +0500

    (webapp): Sign In Page REady.

--- webapp/src/pages/SignIn.tsx
+++ webapp/src/pages/SignIn.tsx
@@ -1,28 +1,47 @@
 import React from "react";
-import "../style/App.css";
+import "../style/SignIn.css";
 
 function SignIn() {
   return (
     <div className="sign-in-page">
       <div className="sign-in-container">
-        <div className="sign-in-box">
-          <h1 className="h1">Sign In To Stack</h1>
-          <form className="input-group">
-            <label>
-              Username:
-              <input type="text" name="username" />
-            </label>
-            <br />
-            <label>
-              Password:
-              <input type="password" name="password" />
-            </label>
-            <br />
-            <button type="submit" className="button">
-              Sign In
-            </button>
-          </form>
+        <h1 className="h1">Sign In To Stack</h1>
+        <button className="google-sign-in">
+          <img
+            src="https://img.icons8.com/color/16/000000/google-logo.png"
+            alt="Google Logo"
+          />
+          Sign in with Google
+        </button>
+        <div className="separator">
+          <span>or</span>
         </div>
+        <form className="sign-in-form">
+          <div className="input-group">
+            <i className="fas fa-user"></i>
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+              required
+            />
+          </div>
+          <div className="input-group">
+            <i className="fas fa-lock"></i>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              required
+            />
+          </div>
+          <a href="#" className="forgot-password">
+            Don't remember your password?
+          </a>
+          <button type="submit" className="sign-in-btn">
+            Sign In
+          </button>
+        </form>
       </div>
     </div>
   );
