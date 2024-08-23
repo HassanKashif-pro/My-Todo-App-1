056941aed0852f36823b6cceb382bceb1faf78ed
Hassan Kashif. pro <hassan.kashif.pro@Gmail.com>
2024-08-23 22:56:16 +0500

    (webapp): Navbar Menu Icon and Logo (Input Bar) Complete.

--- webapp/src/pages/ToDo.tsx
+++ webapp/src/pages/ToDo.tsx
@@ -1,24 +1,23 @@
 import React from "react";
 import "../style/ToDo.css";
 import { Navbar as NavbarBs, Form, FormControl, Button } from "react-bootstrap";
-import { FaBars, FaDotCircle } from "react-icons/fa"; // You can replace with appropriate icons
+import { FaBars } from "react-icons/fa";
 
 function Todo() {
   return (
     <NavbarBs className="navbar">
-      <div className="icon-left">
-        <img src="Logo.png" alt="Logo" className="logo" />
-      </div>
+      <img src="Logo.png" alt="Logo" className="logo" />
+
       <Form className="task-form">
         <FormControl
           type="text"
           placeholder="Add new task"
           className="task-input"
         />
-        <Button variant="outline-light" className="task-button">
-          <FaBars />
-        </Button>
       </Form>
+      <div className="icon-right">
+        <FaBars />
+      </div>
     </NavbarBs>
   );
 }
