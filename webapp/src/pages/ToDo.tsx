98bf7d35dae9810bb93c172d45601dd1eba7f4f8
Hassan Kashif. pro <hassan.kashif.pro@Gmail.com>
2024-08-24 23:26:39 +0500

    (webapp): (Todo) Page edited

--- webapp/src/pages/ToDo.tsx
+++ webapp/src/pages/ToDo.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "../style/ToDo.css";
-import { Navbar as NavbarBs, Form, FormControl, Button } from "react-bootstrap";
+import { Navbar as NavbarBs, Form, FormControl } from "react-bootstrap";
 import { FaBars } from "react-icons/fa";
 
 function Todo() {
   return (
     <NavbarBs className="navbar">
+      {/* Logo on the left */}
       <img src="Logo.png" alt="Logo" className="logo" />
 
+      {/* Task input centered */}
       <Form className="task-form">
         <FormControl
           type="text"
@@ -15,6 +17,8 @@ function Todo() {
           className="task-input"
         />
       </Form>
+
+      {/* Icon on the right */}
       <div className="icon-right">
         <FaBars />
       </div>
