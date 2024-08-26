268615118918cd82f1babcf353d7675154c498ce
Hassan Kashif. pro <hassan.kashif.pro@Gmail.com>
2024-08-26 18:04:28 +0500

    (webapp): Todo Page styling and menu button changes.

--- webapp/src/pages/ToDo.tsx
+++ webapp/src/pages/ToDo.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "../style/ToDo.css";
 import { Navbar as NavbarBs, Form, FormControl } from "react-bootstrap";
 import { FaBars } from "react-icons/fa";
+import { Menu, MenuOption } from "../lib/Menu";
 
+const options: MenuOption[] = [
+  { value: "1", label: "Option 1" },
+  { value: "2", label: "Option 2" },
+  { value: "3", label: "Option 3" },
+];
 function Todo() {
+  const [selectedOption, setSelectedOption] = useState(options[0]);
+
   return (
     <NavbarBs className="navbar">
       {/* Logo on the left */}
@@ -21,6 +29,14 @@ function Todo() {
       {/* Icon on the right */}
       <div className="icon-right">
         <FaBars />
+        <Menu
+          value={selectedOption}
+          onChange={(value: string) => {
+            const option = options.find((opt) => opt.value === value);
+            if (option) setSelectedOption(option);
+          }}
+          options={options}
+        />
       </div>
     </NavbarBs>
   );
