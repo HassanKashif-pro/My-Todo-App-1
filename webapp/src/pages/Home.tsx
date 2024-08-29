e07742909b8ff90d35255b1c547bb4fa839fc74b
Hassan Kashif. pro <hassan.kashif.pro@Gmail.com>
2024-08-29 21:49:34 +0500

    (webapp): Home Page completed.

--- webapp/src/pages/Home.tsx
+++ webapp/src/pages/Home.tsx
@@ -1,10 +1,10 @@
 import "../style/Home.css";
 import { Button, Container } from "react-bootstrap";
-
+import { IoIosArrowDropright } from "react-icons/io";
 function Home() {
   return (
     <Container>
-      <Button className="Useless-btn">Completed</Button>
+      <Button className="Useless-btn">Get Started {">"}</Button>
       <div className="Text">Simple Todo List</div>
       <div className="Text-1">My very own custom todo list</div>
     </Container>
