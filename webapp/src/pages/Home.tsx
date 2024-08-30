dda2d5075758716912b94b3330bc04070bb0473d
Hassan Kashif. pro <hassan.kashif.pro@Gmail.com>
2024-08-30 14:44:32 +0500

    (webapp): TaskCard Initial Files and Initial code.

--- webapp/src/pages/Home.tsx
+++ webapp/src/pages/Home.tsx
@@ -1,10 +1,15 @@
 import "../style/Home.css";
 import { Button, Container } from "react-bootstrap";
-import { IoIosArrowDropright } from "react-icons/io";
+
 function Home() {
   return (
     <Container>
-      <Button className="Useless-btn">Get Started {">"}</Button>
+      <Button
+        onClick={() => (window.location.href = "/ToDo")}
+        className="Useless-btn"
+      >
+        Get Started {">"}
+      </Button>
       <div className="Text">Simple Todo List</div>
       <div className="Text-1">My very own custom todo list</div>
     </Container>
