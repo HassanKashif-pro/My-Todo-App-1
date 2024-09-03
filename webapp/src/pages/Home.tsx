184cf832e3d6c0916b4ad63b904d44fd2715dc2b
Hassan Kashif.pro <hassan.kashif.pro@gmail.com>
2024-09-03 19:57:18 +0500

    (Webapp): (fix) layout fix from bottom.

--- webapp/src/pages/Home.tsx
+++ webapp/src/pages/Home.tsx
@@ -1,18 +1,36 @@
+import Sider from "antd/es/layout/Sider";
+import { Menu } from "../lib/Menu";
 import "../style/Home.css";
-import { Button, Container } from "react-bootstrap";
+import { Breadcrumb, Button, Layout } from "antd";
+
+const { Header, Content, Footer } = Layout;
 
 function Home() {
   return (
-    <Container>
-      <Button
-        onClick={() => (window.location.href = "/ToDo")}
-        className="Useless-btn"
-      >
-        Get Started {">"}
-      </Button>
-      <div className="Text">Simple Todo List</div>
-      <div className="Text-1">My very own custom todo list</div>
-    </Container>
+    <>
+      <Layout style={{ minHeight: "100vh" }}>
+        <Header
+          style={{
+            position: "sticky",
+            top: 0,
+            height: "100px",
+            zIndex: 1,
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <div className="demo-logo" />
+        </Header>
+        <Content style={{ padding: "0 48px" }}>
+          <Button className="Useless-btn">Get Started {">"}</Button>
+          <div className="Text">Simple Todo List</div>
+          <div className="Text-1">My very own custom todo list</div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>Hi I am Footer.</Footer>
+      </Layout>
+    </>
   );
 }
+
 export default Home;
