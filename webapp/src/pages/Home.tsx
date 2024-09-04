01359dbb9f4e3168357dae9b18f30a6eca9ff16d
Hassan Kashif.pro <hassan.kashif.pro@gmail.com>
2024-09-04 16:43:46 +0500

    (Webapp): Logo added in the header.

--- webapp/src/pages/Home.tsx
+++ webapp/src/pages/Home.tsx
@@ -1,7 +1,8 @@
 import Sider from "antd/es/layout/Sider";
 import { Menu } from "../lib/Menu";
 import "../style/Home.css";
-import { Breadcrumb, Button, Layout } from "antd";
+import { Image, Button, Layout } from "antd";
+import { Link } from "react-router-dom";
 
 const { Header, Content, Footer } = Layout;
 
@@ -13,21 +14,23 @@ function Home() {
           style={{
             position: "sticky",
             top: 0,
-            height: "100px",
+            height: "150px",
+            backgroundColor: "#38404b",
             zIndex: 1,
             width: "100%",
             display: "flex",
             alignItems: "center",
           }}
         >
-          <div className="demo-logo" />
+          <Link to="/Home">
+            <Image src="/Logo.png" alt="/Home" preview={false} />
+          </Link>
         </Header>
-        <Content style={{ padding: "0 48px" }}>
+        <Content style={{ padding: "0 48px", backgroundColor: "#38404b" }}>
           <Button className="Useless-btn">Get Started {">"}</Button>
           <div className="Text">Simple Todo List</div>
           <div className="Text-1">My very own custom todo list</div>
         </Content>
-        <Footer style={{ textAlign: "center" }}>Hi I am Footer.</Footer>
       </Layout>
     </>
   );
