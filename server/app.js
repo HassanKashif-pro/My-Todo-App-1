36cebfee0ffeb6aa0a756188dee23654d45e34f4
Hassan Kashif.pro <hassan.kashif.pro@gmail.com>
2024-09-14 19:58:12 +0500

    (server): dotenv installed and set connectoin string.

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 
 // Create an Express app
 const app = express();
